c506ec4832fcdcdd0f31efb04758e0eac8fc3447
notRadioOpaque <ismailmuyideen.c05@gmail.com>
2025-11-19 11:51:48 +0100

    create tsonClass and export instance

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello via Bun!');
+/**
+ * @tson/core
+ *
+ * Core JSON parsing, stringification, and custom type serialization.
+ *
+ * @example
+ * ```typescript
+ * import { Tson } from '@tson/core';
+ *
+ * interface User {
+ *   id: string;
+ *   name: string;
+ *   created: Date;
+ * }
+ *
+ * const user: User = {
+ *   id: '1',
+ *   name: 'Radio',
+ *   created: new Date(),
+ * };
+ *
+ * const json = Tson.stringify(user);
+ * const parsed = Tson.parse<User>(json);
+ * ```
+ */
+
+// exports goes here...
---
+++ packages/core/src/tson.ts
@@ -0,0 +1,21 @@
+/**
+ * Main Tson class
+ *
+ * Type-safe JSON parser and stringifier with custom type support
+ */
+
+/**
+ * Main Tson class
+ */
+export class TsonClass {
+  constructor() {
+    // constructor stuff goes here...
+  }
+
+  // bunch of other methods...
+}
+
+/**
+ * Default Tson instance
+ */
+export const Tson = new TsonClass();
